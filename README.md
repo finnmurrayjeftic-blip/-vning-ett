a3537a1cfd2b9e195c600542cf208001e0fac4f1
finnmurrayjeftic-blip <finnmurray.jeftic@skola.taby.se>
2025-11-06 12:42:37 +0100

    övning 1
    övning 1 med main funktion

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # -vning-ett
+def main() gör huvudkoden och frågar om namn och skriver ut det med str
---
+++ övning1.py
@@ -0,0 +1,6 @@
+def main():
+	name = input("Vad heter du? ")
+	print(f"Hej {name} och välkommen till spelet!")
+
+main()
+
